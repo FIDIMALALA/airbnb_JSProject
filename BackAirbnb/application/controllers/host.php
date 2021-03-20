5384e8eb12de69b81053723a9d69d5e3c79ab6f2
Heri Fiononana <HeryFiononana19@gmail.com>
2021-03-20 10:17:27 -0100

    create post method

--- BackAirbnb/application/controllers/host.php
+++ BackAirbnb/application/controllers/host.php
@@ -6,14 +6,61 @@
     class host extends REST_Controller{
         public function __construct(){
             parent::__construct();
-            $this->load->model('host_Model');
+            // $this->load->model('host_Model');
         }
 
         public function index(){
         }
 
+        public function index_get(){
+            $this->addHost_post();
+        }
+
+        // methode : GET
+        // * @url : host/getHost
         public function getData_get(){
             $data = $this->host_Model->getData();
             $this->response($data, REST_Controller::HTTP_OK);
         }
+
+        // methode : POST
+        public function addHost_post(){
+            header("Access-Control-Allow-Origin: *");
+            $this->form_validation->set_rules('EMailHote', 'hostMail', 'valid_emails');
+
+            if ($this->form_validation->run() == FALSE) {
+                echo "Data not found";
+            }
+            else {
+                $insert_data = [
+                    'NomHote' => $this->post('hostNom'),
+                    'PrenomHote' => $this->post('hostPrenom'),
+                    'DDNHote' => $this->post('hostDDN'),
+                    'EMailHote' => $this->post('hostMail'),
+                    'TelHote' => $this->post('hostTel'),
+                    'PaysHote' => $this->post('hostPays'),
+                ];
+
+                $output = $this->host_Model->save($insert_data);
+
+                if ($output>0 AND !empty($output)) {
+                    $message = [
+                        'status' => true,
+                        'message' => "Host registration successful"
+                    ];
+                    $this->response($message, REST_Controller::HTTP_OK);
+                }else{
+                    $message = [
+                        'status' => true,
+                        'message' => "Not register your account"
+                    ];
+                    $this->response($message, REST_Controller::HTTP_NOT_FOUND);
+                }       
+            }
+        }
+
+        // host login
+        // public function host_login($hostNam, $hostPWD){
+
+        // }
     }
--- frontairbnb/src/App.js
+++ frontairbnb/src/App.js
@@ -1,12 +1,39 @@
-import './App.css';
-import Accueil from './Components/Accueil'
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <Accueil/>
-    </div>
-  );
+class App extends Component {
+
+  async postData() {
+    try {
+      let result = await fetch('http://localhost/airbnb_jsproject/backairbnb/index.php/host/addhost',{
+        method : 'post',
+        mode : 'no-cors',
+        header : {
+          'Accept' : 'application/json',
+          'Content-type' : 'application/json'
+        },
+        body: JSON.stringify({
+          hostNom : 'hery',
+          hostPrenom : 'fiononana',
+          hostDDN : '12-12-1990',
+          hostMail : 'heryfiononana19@gmail.com',
+          hostTel : '345343',
+          hostPays : 'dago'
+        })
+      })
+      console.log(result);
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.postData} >Valider</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
