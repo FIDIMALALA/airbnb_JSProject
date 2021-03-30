026b99c1510644389cf43bb60cc131f836727ac2
Heri Fiononana <HeryFiononana19@gmail.com>
2021-03-30 11:33:58 +0000

    modified method addHost_post

--- BackAirbnb/application/controllers/host.php
+++ BackAirbnb/application/controllers/host.php
@@ -6,7 +6,6 @@
     class host extends REST_Controller{
         public function __construct(){
             parent::__construct();
-            // $this->load->model('host_Model');
         }
 
         public function index(){
@@ -17,32 +16,30 @@
         }
 
         // methode : GET
-        // * @url : host/getHost
+        // @url : host/getHost
         public function getData_get(){
             $data = $this->host_Model->getData();
             $this->response($data, REST_Controller::HTTP_OK);
         }
 
         // methode : POST
+        // @url : host/addHost
         public function addHost_post(){
             header("Access-Control-Allow-Origin: *");
             $this->form_validation->set_rules('EMailHote', 'hostMail', 'valid_emails');
-
             if ($this->form_validation->run() == FALSE) {
                 echo "Data not found";
             }
             else {
-                $insert_data = [
-                    'NomHote' => $this->post('hostNom', TRUE),
-                    'PrenomHote' => $this->post('hostPrenom', TRUE),
-                    'DDNHote' => $this->post('hostDDN', TRUE),
-                    'EMailHote' => $this->post('hostMail', TRUE),
-                    'TelHote' => $this->post('hostTel', TRUE),
-                    'PaysHote' => $this->post('hostPays', TRUE),
-                ];
-
+                $insert_data = array(
+                    'NomHote' => $this->input->get('hostName', FALSE),
+                    'PrenomHote' => $this->input->get('hostLastName', FALSE),
+                    'DDNHote' => $this->input->get('hostBorn', FALSE),
+                    'EMailHote' => $this->input->get('hostMail', FALSE),
+                    'TelHote' => $this->input->get('hostTel', FALSE),
+                    'PaysHote' => $this->input->get('hostPays', FALSE)
+                );
                 $output = $this->host_Model->save($insert_data);
-
                 if ($output>0 AND !empty($output)) {
                     $message = [
                         'status' => true,
